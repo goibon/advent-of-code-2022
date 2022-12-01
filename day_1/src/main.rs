0e4e840a5dd158f1e2d842cefc2be735651dc437
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2022-12-01 21:33:24 +0100

    Added solution to day 1 part 2.

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -18,6 +18,15 @@ fn part_1(input: &str) -> u32 {
     *calories_sums.last().unwrap()
 }
 
+fn part_2(input: &str) -> u32 {
+    let mut calories_sums = split_input(input)
+        .iter()
+        .map(|set| set.iter().sum::<u32>())
+        .collect::<Vec<u32>>();
+    calories_sums.sort_unstable();
+    calories_sums.iter().rev().take(3).sum()
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let path = &args[1];
@@ -26,6 +35,9 @@ fn main() {
 
     let part_1_result = part_1(&input);
     println!("Day 1 Part 1: {}", part_1_result);
+
+    let part_2_result = part_2(&input);
+    println!("Day 1 Part 2: {}", part_2_result);
 }
 
 #[cfg(test)]
@@ -44,5 +56,12 @@ mod tests {
         let test_input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000\n";
 
         assert_eq!(part_1(test_input), 24000);
+    }
+
+    #[test]
+    fn part_2_should_return_the_sum_of_the_top_three_highest_sums() {
+        let test_input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000\n";
+
+        assert_eq!(part_2(test_input), 45000);
     }
 }
