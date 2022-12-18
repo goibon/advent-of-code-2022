b73096c398d3d24866d6a0c7eec4ad149cbaf317
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2022-12-18 20:15:24 +0100

    Rewrote day 9 part_1 to follow a more functional style.

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -14,7 +14,9 @@ fn touches(head_x: i32, head_y: i32, tail_x: i32, tail_y: i32) -> bool {
 }
 
 fn part_1(input: &str) -> usize {
-    let moves = input
+    let mut current_head_position = (0, 0);
+    let mut current_tail_position = (0, 0);
+    input
         .lines()
         .filter_map(|line| line.split_once(' '))
         .filter_map(|(direction, distance)| {
@@ -30,54 +32,52 @@ fn part_1(input: &str) -> usize {
             "L" => Some(Move::Left(distance)),
             "R" => Some(Move::Right(distance)),
             _ => None,
-        });
-
-    let mut visited_positions = HashSet::<(i32, i32)>::from([(0, 0)]);
-    let mut current_head_position = (0, 0);
-    let mut current_tail_position = (0, 0);
-    for movement in moves {
-        let (mut head_x, mut head_y) = current_head_position;
-        let (mut tail_x, mut tail_y) = current_tail_position;
-        let mut x_step = 0;
-        let mut y_step = 0;
-        match movement {
-            Move::Up(distance) => {
-                head_y += distance;
-                y_step = 1;
-            }
-            Move::Down(distance) => {
-                head_y -= distance;
-                y_step = -1;
-            }
-            Move::Left(distance) => {
-                head_x -= distance;
-                x_step = -1;
-            }
-            Move::Right(distance) => {
-                head_x += distance;
-                x_step = 1;
-            }
-        };
-
-        while !touches(head_x, head_y, tail_x, tail_y) {
-            tail_x += x_step;
-            tail_y += y_step;
-            // Diagonal move required
-            if head_x != tail_x && head_y != tail_y {
-                if matches!(movement, Move::Up(_)) || matches!(movement, Move::Down(_)) {
-                    tail_x += head_x - tail_x;
-                } else {
-                    tail_y += head_y - tail_y;
+        })
+        .flat_map(|movement| {
+            let mut visited_positions = vec![(0, 0)];
+            let (mut head_x, mut head_y) = current_head_position;
+            let (mut tail_x, mut tail_y) = current_tail_position;
+            let mut x_step = 0;
+            let mut y_step = 0;
+            match movement {
+                Move::Up(distance) => {
+                    head_y += distance;
+                    y_step = 1;
                 }
+                Move::Down(distance) => {
+                    head_y -= distance;
+                    y_step = -1;
+                }
+                Move::Left(distance) => {
+                    head_x -= distance;
+                    x_step = -1;
+                }
+                Move::Right(distance) => {
+                    head_x += distance;
+                    x_step = 1;
+                }
+            };
+
+            while !touches(head_x, head_y, tail_x, tail_y) {
+                tail_x += x_step;
+                tail_y += y_step;
+                // Diagonal move required
+                if head_x != tail_x && head_y != tail_y {
+                    if matches!(movement, Move::Up(_)) || matches!(movement, Move::Down(_)) {
+                        tail_x += head_x - tail_x;
+                    } else {
+                        tail_y += head_y - tail_y;
+                    }
+                }
+                visited_positions.push((tail_x, tail_y));
             }
-            visited_positions.insert((tail_x, tail_y));
-        }
 
-        current_head_position = (head_x, head_y);
-        current_tail_position = (tail_x, tail_y);
-    }
-
-    visited_positions.len()
+            current_head_position = (head_x, head_y);
+            current_tail_position = (tail_x, tail_y);
+            visited_positions
+        })
+        .collect::<HashSet<(i32, i32)>>()
+        .len()
 }
 
 fn main() {
